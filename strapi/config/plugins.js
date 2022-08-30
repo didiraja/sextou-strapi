d8851c3c16531bcd4a5fc25d20e4b48eaa7cf5e0
didiraja <didiraja@hotmail.com>
2022-08-30 16:29:46 -0300

    feat: 🎸 included documentation and generate data plugin

--- strapi/config/plugins.js
+++ strapi/config/plugins.js
@@ -1,5 +1,8 @@
 module.exports = () => ({
     'duplicate-button': true,
+    'generate-data': {
+        enabled: true,
+    },
     // "custom-api": {
     //     enabled: true,
     //     resolve: "./src/plugins/custom-api",
